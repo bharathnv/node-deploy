7bfd90583b8a8f98478f0b016c8a7877f38f8174
bharathnv <bharathnadignv4@@gmail.com>
2018-10-27 08:12:00 +0530

    port environment var for port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/makeCall', urlencodedParser, function (req, res) {
   });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
    var host = server.address().address
    var port = server.address().port
    
